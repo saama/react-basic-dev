35e91a648dceeebb1b9058eed80191eb817220b9
saama <hjpw237@gmail.com>
2020-06-10 11:22:08 +0900

    setting

---
+++ src/store/actionType/actionTypes.js
@@ -0,0 +1,5 @@
+// 액션 타입 정의
+export const INCREMENT = 'counter/INCREMENT' ;
+export const DECREMENT = 'counter/DECREMENT' ;
+export const TEST = 'counter/TEST' ;
+export const ACTIONTEST = 'counter/ACTIONTEST' ;
